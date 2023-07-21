c631cafac02df0394ff9b4a3a65cf49d0e578299
Maksim Popov <115405460+MamkaTvoya228@users.noreply.github.com>
2023-07-21 10:45:04 +0300

    Just lern

---
+++ Lolz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("Hel");
+    int x;
+    printf("Enter value\n");
+    scanf("%d",&x);
+    printf("Here %d",x);
+}
